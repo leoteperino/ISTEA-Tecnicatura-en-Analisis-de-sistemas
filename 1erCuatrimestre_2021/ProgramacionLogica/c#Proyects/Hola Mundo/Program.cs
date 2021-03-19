c17319a721f3c6224e95ed70963231aa8a4d8670
Leandro <leoteperino@gmail.com>
2021-03-19 19:59:14 -0300

    first commit

---
+++ 1erCuatrimestre_2021/ProgramacionLogica/c#Proyects/Hola Mundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Presiona una tecla para salir");
+            Console.ReadKey();
+
+            int a = 5;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine("El resultado de "+a+" + "+b+" es: "+c);
+            Console.Read(); 
+        }
+    }
+}
