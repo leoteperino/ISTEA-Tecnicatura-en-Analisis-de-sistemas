200d13f1aa6c41be223814dce88740685c4986fb
Leandro <leoteperino@gmail.com>
2021-03-30 21:07:12 -0300

    Update

---
+++ 1erCuatrimestre_2021/EstructuraDeDatosYdiseñoComputacional/c#program/Clase1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Clase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int? a=null;        
+            if(a!=null) 
+            {
+                Console.WriteLine("Es igual a null");
+            }
+            else
+            {
+                Console.WriteLine("No es igual a null");
+            }
+
+            string texto1="Nicolas";
+            string texto2="Nicolas";
+            bool result=texto1.Equals(texto2);
+            if (result==true)
+            {
+                Console.WriteLine("Ambas cadenas son iguales");
+            }
+            else
+            {
+                Console.WriteLine("Ambas cadenas no son iguales");
+            }
+
+            int b=17;
+            switch(b)
+            {
+                case 15:
+                    Console.WriteLine("El valor es 15");
+                    break; 
+                case 16:
+                    Console.WriteLine("El valor es 16");
+                    break;     
+                case 17:
+                    Console.WriteLine("El valor es 17");
+                break; 
+            }
+        }
+    }
+}
